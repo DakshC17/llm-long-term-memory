c26dd397bee53a47ba311795354a3702ce3b78d1
daksh <dakshc1709@gmail.com>
2025-08-05 21:04:19 +0530

    Trying for a cli assistant

---
+++ cli_memory_assistant.py
@@ -0,0 +1,9 @@
+from memory_storage import add_memory, get_memories, delete_memory
+from memory_agent import recall_with_gemini
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+# Now i am trying to make it a cli interface which will make it more interactive for users 
+# I can make it streamlit or CLi both best fit will be the one
+
